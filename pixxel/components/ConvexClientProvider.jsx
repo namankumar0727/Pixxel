d50fd80ef53ddbdbf4dd58bdfe950fa9ecc46e8a
namankumar0727 <namankumar0727@gmail.com>
2025-09-09 01:16:54 +0530

    12

--- pixxel/components/ConvexClientProvider.jsx
+++ pixxel/components/ConvexClientProvider.jsx
@@ -5,7 +5,7 @@ import { ConvexReactClient } from "convex/react";
 import { useAuth } from "@clerk/nextjs";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
+const convex =  ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
 
 export function ConvexClientProvider({ children }) {
   return (
